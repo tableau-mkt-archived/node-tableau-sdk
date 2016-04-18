9b3efd768e1c73c1f1cde20a3ff86b8c832d8054
Eric Peterson <epeterson@tableausoftware.com>
2016-04-18 13:51:01 -0700

    Install the SDK on Mac builds.

--- scripts/install-sdk.sh
+++ scripts/install-sdk.sh
@@ -2,11 +2,13 @@
 
 # Install the SDK for linux
 if [ $TRAVIS_OS_NAME == 'linux' ]; then
-  wget --directory-prefix=$HOME/tableau-sdk https://downloads.tableau.com/tssoftware/Tableau-SDK-Linux-64Bit-9-3-0.deb
-  sudo dpkg -i $HOME/tableau-sdk/Tableau-SDK-Linux-64Bit-9-3-0.deb
+  wget --directory-prefix=$HOME/tableau-c-sdk https://downloads.tableau.com/tssoftware/Tableau-SDK-Linux-64Bit-9-3-0.deb
+  sudo dpkg -i $HOME/tableau-c-sdk/Tableau-SDK-Linux-64Bit-9-3-0.deb
 fi
 
 # Install the SDK for OSX.
 if [ $TRAVIS_OS_NAME == 'osx' ]; then
-  echo "@todo"
+  wget --directory-prefix=$HOME/tableau-c-sdk https://downloads.tableau.com/tssoftware/Tableau-SDK-9-3-0.dmg
+  sudo hdiutil attach $HOME/tableau-c-sdk/Tableau-SDK-9-3-0.dmg
+  sudo cp -r /Volumes/Tableau\ SDK/Frameworks/ /Library/Frameworks/
 fi
