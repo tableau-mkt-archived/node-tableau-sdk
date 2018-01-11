88ef6bb722da6f4964c397fdb2ed0e2f0d5d007a
Eric Peterson <iamEAP@users.noreply.github.com>
2018-01-10 19:28:00 -0800

    Point at 10.5.0 Extract API files

--- scripts/install-sdk.sh
+++ scripts/install-sdk.sh
@@ -4,13 +4,13 @@ LOCAL_SDK_VERSION=${TABLEAU_SDK_VERSION:-10-2-0}
 
 # Install the SDK for linux
 if [ $TRAVIS_OS_NAME == 'linux' ]; then
-  wget --directory-prefix=$HOME/tableau-c-sdk https://downloads.tableau.com/tssoftware/Tableau-SDK-Linux-64Bit-${LOCAL_SDK_VERSION}.deb
-  sudo dpkg -i $HOME/tableau-c-sdk/Tableau-SDK-Linux-64Bit-${LOCAL_SDK_VERSION}.deb
+  wget --directory-prefix=$HOME/tableau-c-sdk https://downloads.tableau.com/tssoftware/extractapi-linux-x86_64-${LOCAL_SDK_VERSION}.deb
+  sudo dpkg -i $HOME/tableau-c-sdk/extractapi-linux-x86_64-${LOCAL_SDK_VERSION}.deb
 fi
 
 # Install the SDK for OSX.
 if [ $TRAVIS_OS_NAME == 'osx' ]; then
-  wget --directory-prefix=$HOME/tableau-c-sdk https://downloads.tableau.com/tssoftware/Tableau-SDK-${LOCAL_SDK_VERSION}.dmg
-  sudo hdiutil attach $HOME/tableau-c-sdk/Tableau-SDK-${LOCAL_SDK_VERSION}.dmg
+  wget --directory-prefix=$HOME/tableau-c-sdk https://downloads.tableau.com/tssoftware/extractapi-${LOCAL_SDK_VERSION}.dmg
+  sudo hdiutil attach $HOME/tableau-c-sdk/extractapi-${LOCAL_SDK_VERSION}.dmg
   sudo ditto /Volumes/Tableau\ SDK/Frameworks/ /Library/Frameworks/
 fi
